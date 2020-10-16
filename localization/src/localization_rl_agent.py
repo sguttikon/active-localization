f6f47153d18ee3c6e4166d08db070cd84d2bd156
suresh-guttikonda <guttikondasuresh@gmail.com>
2020-10-16 16:00:07 +0200

    pretrained ppo agent

--- localization/src/localization_rl_agent.py
+++ localization/src/localization_rl_agent.py
@@ -12,19 +12,30 @@ set_path('/media/suresh/research/awesome-robotics/active-slam/catkin_ws/src/open
 from openai_ros.task_envs.turtlebot3 import turtlebot3_localize
 import gym
 import rospy
+import argparse
 
 from stable_baselines3 import DQN
 from stable_baselines3 import PPO
 
-def train_network():
+def train_network(file_path: str):
+    """
+    Train the RL agent for localization task and store the policy/agent
+
+    :params str file_path: location to store the trained agent
+    """
     model = PPO('MlpPolicy', env, verbose=1)
     model.learn(total_timesteps=25000)
 
-    model.save("ppo_turtlebot3_localize")
+    model.save(file_path)
     print('training finished')
 
-def eval_network():
-    model = PPO.load("ppo_turtlebot3_localize")
+def eval_network(file_path: str):
+    """
+    Evaluate the pretrained RL agent for localization task
+
+    :params str file_path: location to load the pretrained agent
+    """
+    model = PPO.load(file_path)
 
     obs = env.reset()
     for i in range(1000):
@@ -39,14 +50,19 @@ def eval_network():
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser(description='Train/Evaluate localization RL agent')
+    parser.add_argument('--file_path', dest='file_path', \
+                    required=True, help='full path for location to store/load agent')
+    args = parser.parse_args()
+
     # create a new ros node
     rospy.init_node('turtlebot3_localization')
 
     # create a new gym turtlebot3 localization environment
     env = gym.make('TurtleBot3Localize-v0')
 
-    #train_network()
-    eval_network()
+    #train_network(args.file_path)
+    eval_network(args.file_path)
 
     # prevent te code from exiting until an shutdown signal (ctrl+c) is received
     rospy.spin()
